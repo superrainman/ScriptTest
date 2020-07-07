3b0fa021ef660e465abff88f63071ed5427d6143
奥山翔平 <amewotoko@gmail.com>
2020-07-07 20:57:21 +0900

    Lesson4 課題
    発展課題まで完了。通常課題は「Challenge1」発展課題は「Challenge2」という名前のスクリプトで編集しています。

---
+++ Assets/Challenge1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Challenge1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		int[] array = {2,3,5,7,11};
+
+		//正順表示
+		for (int i = 0; i < 5; i++) 
+		{
+			Debug.Log(array[i]);
+		}//End of for
+
+		//逆順表示
+		for (int i = 4; i >= 0; i--) 
+		{
+			Debug.Log(array[i]);
+		} //End of for
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Challenge2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+	private int hp = 100;
+	private int power = 25;
+	private int mp = 53;
+
+	public void Attack()
+	{
+		Debug.Log(this.power + "のダメージを与えた！");
+	}
+
+	public void Defence(int damage)
+	{
+		Debug.Log(damage + "のダメージを受けた！");
+		this.hp -= damage;
+	}
+
+	public void Magic()
+	{
+		this.mp -= 5;
+		if (this.mp >= 5)
+		{
+			Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+		}
+		else 
+		{
+			Debug.Log("MPが足りないため魔法が使えない。");
+		}
+	}
+}
+
+public class Challenge2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Boss lastboss = new Boss();
+		lastboss.Attack();
+		lastboss.Defence(3);
+
+		for (int i=0; i<10; i++)
+		{
+			lastboss.Magic();
+		}
+
+	} //Start End
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
